02acd0394c80261576c2ade04f0d57666403c545
Ian Wheeler <iwheeler@micron.com>
2021-05-14 17:53:05 -0700

    adding multiple tmp and dests

--- plotBoy.py
+++ plotBoy.py
@@ -21,8 +21,8 @@ class winPC(object):
         # Hardcode CPU_core and RAM_MB for your system!
         # TODO: have python figure out the CPU_core and RAM_MB details
         self.chia_path = "C:\\Users\\ssdrive\\AppData\\Local\\chia-blockchain\\app-1.1.5\\resources\\app.asar.unpacked\\daemon\\"
-        self.CPU_cores = 8
-        self.RAM_MB = 30000
+        self.CPU_cores = 10
+        self.RAM_MB = 38000
         
         # Search for Temp and Destination Paths. Requirements:
         #     Temp has a folder named 'plot'
@@ -44,17 +44,12 @@ class winPC(object):
                 
             # Check for Temp disks
             if(os.path.isdir(memory.device + "plot")):
-                self.tmp_memory.append(memory)
-                # print str(memory.device) + " found as a temp disk!"
-            # else:
-                # print str(memory.device) + " not a temp disk!"
+                # TODO: Need mitigation plan if device's memory is less than 300GB
+                self.tmp_memory.append(memoryClass(memory, isPlotter=True))
             
             # Check for Temp disks
             if(os.path.isdir(memory.device + "farm")):
-                self.dest_memory.append(memory)
-                # print str(memory.device) + " found as a farm disk!"
-            # else:
-                # print str(memory.device) + " not a farm disk!"
+                self.dest_memory.append(memoryClass(memory, isPlotter=False))
         
         # Print tmp and dest locations
         print "\nSummary:"
@@ -62,13 +57,13 @@ class winPC(object):
         for x in range(0, len(self.tmp_memory)):
             if x != 0:
                 tmp_print += ", "
-            tmp_print += str(self.tmp_memory[x].device)
+            tmp_print += str(self.tmp_memory[x].memory.device)
         print(tmp_print)
         dest_print = "dest locations: "
         for x in range(0, len(self.dest_memory)):
             if x != 0:
                 dest_print += ", "
-            dest_print += str(self.dest_memory[x].device)
+            dest_print += str(self.dest_memory[x].memory.device)
         print(dest_print)
         
         # Calculate best plotting values
@@ -77,42 +72,94 @@ class winPC(object):
     def calculate_best_plotting(self):
         # values to set
         self.total_processes = self.CPU_cores
+        self.mem_per_process = 0
+        self.stringCmds = []
     
         # TODO: Clean up plot directory
     
         # 1) Find the best amount of processes to run in parallel
         total_tmp_mem = 0
         for memory in self.tmp_memory:
-            usage = psutil.disk_usage(memory.mountpoint)
-            total_tmp_mem += usage.free
+            total_tmp_mem += memory.usage.free
         # print "total_tmp_mem = " + str(total_tmp_mem)
         process_max_size = 300*1024*2024 * self.CPU_cores       # 300GB tmp space per plot in parallel
         while process_max_size > total_tmp_mem:
             self.total_processes = self.total_processes - 1
-            process_max_size = process_max_size - 300*1024*2024
+            process_max_size = process_max_size - 300*1024*1024
             if process_max_size < 0:
                 raise ValueError("Not enough temp space to plot!")
+        print "Total Concurrent Plot Processes: " + str(self.total_processes)
         
-                
         # 2) Find the best amount of memory to utilize
         self.mem_per_process = int(self.RAM_MB/ self.total_processes)
-        if(self.mem_per_process > 3400):        # 3400 is max size noted on chia.net - https://www.chia.net/2021/02/22/plotting-basics.html
-            self.mem_per_process = 3400
+        if(self.mem_per_process > 4000):        # 3400 is max size noted on chia.net - https://www.chia.net/2021/02/22/plotting-basics.html, but have seen examples of more. Max at 4000?
+            self.mem_per_process = 4000
         print "mem_per_process = " + str(self.mem_per_process)
         
         # 3) Find total plots to fill up destination memory
-        total_dest_mem = 0
+        self.total_plots = 0
         for memory in self.dest_memory:
-            usage = psutil.disk_usage(memory.mountpoint)
-            total_dest_mem += usage.free
-        self.total_plots = int(float(total_dest_mem) / 116823110452)       # Each plot is 108.8GB, or 114085068 bytes
+            self.total_plots += memory.totalplots
+        # self.total_plots = int(float(total_dest_mem) / 116823110452)       # Each plot is 108.8GB, or 114085068 bytes
         # print "total_dest_mem = " + str(total_dest_mem) + ", or " + str(float(total_dest_mem) / (1024*1024*1024*1024*1024)) + "TB"
         # print "Each plot is 108.8GB, or " + str(108.8*1024*1024*1024) + " bytes"
         print "Need " + str(self.total_plots) + " plots to fill up destination disk space"
         
-        # Final: Set run strings
-        # TODO: Need to add in multiple tmp_paths
-        self.stringCmd = self.chia_path + "chia.exe plots create -k 32 -b " + str(self.mem_per_process) + " -u 128 -r 2 -t " + str(self.tmp_memory[0].device + "plot") + " -d " + str(self.dest_memory[0].device + "farm") + " -n 1"
+        # Initialize destination plot counts
+        destplotcount = []
+        for x in range(0, len(self.dest_memory)):
+            destplotcount.append(self.dest_memory[x].totalplots)
+            
+        # Initialize tmp list
+        tmpPlotList = []
+        while(len(tmpPlotList) < self.total_plots):
+            tmpPlotCnt = [1]*len(self.tmp_memory)
+            processCnt = 0
+            # Balance per process group
+            while(processCnt < self.total_processes):
+                for x in range(0, len(self.tmp_memory)):
+                    if not(tmpPlotCnt[x] > self.tmp_memory[x].totalplots):
+                        tmpPlotList.append(x)
+                        tmpPlotCnt[x] += 1
+                        processCnt += 1
+        
+        # Calculate plot command strings
+        totalpltcount = 0
+        while totalpltcount < self.total_plots:
+            for x in range(0, len(self.dest_memory)):
+                if(destplotcount[x] > 0):
+                    self.stringCmds.append(self.chia_path + "chia.exe plots create -k 32 -b " + str(self.mem_per_process) + " -u 128 -r 2 -t " + str(self.tmp_memory[tmpPlotList[totalpltcount]].memory.device + "plot") + " -d " + str(self.dest_memory[x].memory.device + "farm") + " -n 1")
+                    destplotcount[x] -= 1
+                    totalpltcount += 1
+        
+        # print ""
+        # for x in range(0, len(self.stringCmds)):
+            # print str(x+1) + " " + self.stringCmds[x]
+        
+class memoryClass(object):
+    def __init__(self, psutil_memclass, isPlotter):
+        self.memory = psutil_memclass
+        self.usage = psutil.disk_usage(self.memory.mountpoint)
+        self.isPlotter = isPlotter
+        if self.isPlotter:
+            self.isDestination = False
+        else:
+            self.isDestination = True
+        self.calculateTotalPlots()
+        
+    def calculateTotalPlots(self):
+        if self.isPlotter:
+            # Need 300GB for plotters
+            self.totalplots = int(float(self.usage.free) / 322122547200)
+        else:
+            # Need 108.8GB for destination plot
+            self.totalplots = int(float(self.usage.free) / 116823110452)
+        # print str(self.memory.device) + " has " + str(self.usage.free) + " space for " + str(self.totalplots) + " total plots"
+            
+    def recalculateUsage(self):
+        self.usage = psutil.disk_usage(memory.mountpoint)
+        self.calculateTotalPlots()
+        
         
 class winWorkloadThread(threading.Thread):
     def __init__(self, string_command, workerID):
@@ -121,12 +168,12 @@ class winWorkloadThread(threading.Thread):
 
     def run(self):
         new_string = "start /wait C:\\Windows\\system32\\cmd.exe /c " + self.what_to_run_string + ""
-        print new_string
+        print new_string + "\n"
         os.system(new_string)
 
         
 # Works on Micron network only!
-def SendEmail(receiver, subject, message, senderName="iwheeler"):
+def SendEmail(receiver, subject, message, senderName="youremail"):
     sender = "relay.micron.com"
 
     header = "To:" + str(receiver) + "\nFrom:" + senderName + "\nSubject:" + subject + "\n"
@@ -140,15 +187,19 @@ def SendEmail(receiver, subject, message, senderName="iwheeler"):
 # Main                                                    #
 ###########################################################
 def main(argv):
+    # TODO: Use logger to capture time
+
     # Command line arguments
     parser = argparse.ArgumentParser(description='ERROR: Invalid inputs')
     parser.add_argument('--mNetworkEmail',default=False,type=bool,metavar="mNetworkEmail",help="Use this flag if you want email updates on plots")
     parser.add_argument('--staggerMin',default=10,type=int,metavar="staggerMin",help="Stagger time between plots in minutes")
+    parser.add_argument('--email',default="youremail@gmail.com",type=str,metavar="email",help="Stagger time between plots in minutes")
     args = parser.parse_args()
 
     # Get arguments
     emailUpdateFlag = args.mNetworkEmail
     staggerTimeMin = args.staggerMin
+    emailAddress = args.email
 
     # Determine if Windows / Linux
     if sys.platform.startswith('win'):
@@ -161,35 +212,38 @@ def main(argv):
     WorkerID = 1
     PlotCompleted = 1
     for x in range(0, thisPC.total_processes):
-        workloads.append(winWorkloadThread(thisPC.stringCmd, "WorkerID " + str(WorkerID)))
-        print "Plot " + str(WorkerID) + " started"
+        workloads.append(winWorkloadThread(thisPC.stringCmds[WorkerID-1], "WorkerID " + str(WorkerID)))
         WorkerID += 1
         
     # Kick off plotting processes
+    plotID = 1
     for workload in workloads:
+        print "Plot " + str(plotID) + " started"
         workload.start()
+        plotID += 1
         # TODO: Stagger by plotting stage
         time.sleep(staggerTimeMin * 60)       # Stagger time in minutes
         
     # Monitor Workloads
-    
     while WorkerID < thisPC.total_plots:
         # Check every 10 min
         time.sleep(10*60)
+        # time.sleep(60)
         for x in range(0, len(workloads)):
             if not workloads[x].is_alive():
                 # Email that plot is done if email flag is set
                 if emailUpdateFlag:
                     emailMsg = "Plot " + str(PlotCompleted) + " is done on " + str(socket.gethostname()) + "!"
-                    SendEmail(receiver="iwheeler@micron.com", subject=emailMsg, message=emailMsg, senderName="iwheeler@micron.com")
+                    SendEmail(receiver=emailAddress, subject=emailMsg, message=emailMsg, senderName=emailAddress)
                 # print "workloads[" + str(x) + "] has finished"
                 workloads.remove(workloads[x])
-                thisWorkload = winWorkloadThread(thisPC.stringCmd, "WorkerID " + str(WorkerID))
+                thisWorkload = winWorkloadThread(thisPC.stringCmds[WorkerID-1], "WorkerID " + str(WorkerID))
+                print "Plot " + str(plotID) + " started"
                 thisWorkload.start()
                 workloads.append(thisWorkload)
-                print "Plot " + str(WorkerID) + " started"
                 WorkerID += 1
-                PlotCompleted +=1
+                plotID += 1
+                PlotCompleted += 1
                 break
             # else:
                 # print "workloads[" + str(x) + "] still running"
