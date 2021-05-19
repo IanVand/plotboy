e535408933c1b3eb673690452fe62ea70286b453
Ian Wheeler <iwheeler@micron.com>
2021-05-18 17:57:14 -0700

    added logger; added DEBUG flag; fixed off-by-1 for plotting; fixed monitoring plots until completion

--- plotBoy.py
+++ plotBoy.py
@@ -15,13 +15,16 @@ from psutil._common import bytes2human
 import smtplib
 import argparse
 
+# Global Flags
+DEBUG = False
 
 class winPC(object):
-    def __init__(self):
+    def __init__(self, logger, parallelPlots=10):
         # Hardcode CPU_core and RAM_MB for your system!
         # TODO: have python figure out the CPU_core and RAM_MB details
         self.chia_path = "C:\\Users\\ssdrive\\AppData\\Local\\chia-blockchain\\app-1.1.5\\resources\\app.asar.unpacked\\daemon\\"
-        self.CPU_cores = 10
+        self.parallelPlots = parallelPlots
+        self.logger = logger
         self.RAM_MB = 38000
         
         # Search for Temp and Destination Paths. Requirements:
@@ -45,11 +48,12 @@ class winPC(object):
             # Check for Temp disks
             if(os.path.isdir(memory.device + "plot")):
                 # TODO: Need mitigation plan if device's memory is less than 300GB
-                self.tmp_memory.append(memoryClass(memory, isPlotter=True))
+                self.tmp_memory.append(memoryClass(memory, isPlotter=True, logger=logger))
             
             # Check for Temp disks
             if(os.path.isdir(memory.device + "farm")):
-                self.dest_memory.append(memoryClass(memory, isPlotter=False))
+                if not re.search('E', memory.device, re.I):     # skip this
+                    self.dest_memory.append(memoryClass(memory, isPlotter=False, logger=logger))
         
         # Print tmp and dest locations
         print "\nSummary:"
@@ -71,30 +75,32 @@ class winPC(object):
         
     def calculate_best_plotting(self):
         # values to set
-        self.total_processes = self.CPU_cores
+        self.total_processes = self.parallelPlots
         self.mem_per_process = 0
         self.stringCmds = []
     
         # TODO: Clean up plot directory
+        
+        # TODO: Optimize for disk space with k=32 and k=33 plotting
     
         # 1) Find the best amount of processes to run in parallel
         total_tmp_mem = 0
         for memory in self.tmp_memory:
             total_tmp_mem += memory.usage.free
         # print "total_tmp_mem = " + str(total_tmp_mem)
-        process_max_size = 300*1024*2024 * self.CPU_cores       # 300GB tmp space per plot in parallel
+        process_max_size = 300*1024*2024 * self.parallelPlots       # 300GB tmp space per plot in parallel
         while process_max_size > total_tmp_mem:
             self.total_processes = self.total_processes - 1
             process_max_size = process_max_size - 300*1024*1024
             if process_max_size < 0:
                 raise ValueError("Not enough temp space to plot!")
-        print "Total Concurrent Plot Processes: " + str(self.total_processes)
+        self.logger.info("Total Concurrent Plot Processes: " + str(self.total_processes))
         
         # 2) Find the best amount of memory to utilize
         self.mem_per_process = int(self.RAM_MB/ self.total_processes)
         if(self.mem_per_process > 4000):        # 3400 is max size noted on chia.net - https://www.chia.net/2021/02/22/plotting-basics.html, but have seen examples of more. Max at 4000?
             self.mem_per_process = 4000
-        print "mem_per_process = " + str(self.mem_per_process)
+        self.logger.info("mem_per_process = " + str(self.mem_per_process))
         
         # 3) Find total plots to fill up destination memory
         self.total_plots = 0
@@ -103,7 +109,7 @@ class winPC(object):
         # self.total_plots = int(float(total_dest_mem) / 116823110452)       # Each plot is 108.8GB, or 114085068 bytes
         # print "total_dest_mem = " + str(total_dest_mem) + ", or " + str(float(total_dest_mem) / (1024*1024*1024*1024*1024)) + "TB"
         # print "Each plot is 108.8GB, or " + str(108.8*1024*1024*1024) + " bytes"
-        print "Need " + str(self.total_plots) + " plots to fill up destination disk space"
+        self.logger.info("Need " + str(self.total_plots) + " plots to fill up destination disk space")
         
         # Initialize destination plot counts
         destplotcount = []
@@ -128,19 +134,23 @@ class winPC(object):
         while totalpltcount < self.total_plots:
             for x in range(0, len(self.dest_memory)):
                 if(destplotcount[x] > 0):
-                    self.stringCmds.append(self.chia_path + "chia.exe plots create -k 32 -b " + str(self.mem_per_process) + " -u 128 -r 2 -t " + str(self.tmp_memory[tmpPlotList[totalpltcount]].memory.device + "plot") + " -d " + str(self.dest_memory[x].memory.device + "farm") + " -n 1")
+                    cmd = self.chia_path + "chia.exe plots create -k 32 -b " + str(self.mem_per_process) + " -u 128 -r 2 -t " + str(self.tmp_memory[tmpPlotList[totalpltcount]].memory.device + "plot") + " -d " + str(self.dest_memory[x].memory.device + "farm") + " -n 1"
+                    if DEBUG:
+                        cmd = "echo '" + cmd + "'"
+                    self.stringCmds.append(cmd)
                     destplotcount[x] -= 1
                     totalpltcount += 1
         
-        # print ""
-        # for x in range(0, len(self.stringCmds)):
-            # print str(x+1) + " " + self.stringCmds[x]
+        for x in range(0, len(self.stringCmds)):
+            self.logger.debug(str(x+1) + " " + self.stringCmds[x])
         
 class memoryClass(object):
-    def __init__(self, psutil_memclass, isPlotter):
+    # TODO: make a superclass of psutil_memclass... duh
+    def __init__(self, psutil_memclass, isPlotter, logger):
         self.memory = psutil_memclass
         self.usage = psutil.disk_usage(self.memory.mountpoint)
         self.isPlotter = isPlotter
+        self.logger = logger
         if self.isPlotter:
             self.isDestination = False
         else:
@@ -154,7 +164,8 @@ class memoryClass(object):
         else:
             # Need 108.8GB for destination plot
             self.totalplots = int(float(self.usage.free) / 116823110452)
-        # print str(self.memory.device) + " has " + str(self.usage.free) + " space for " + str(self.totalplots) + " total plots"
+
+        self.logger.debug(str(self.memory.device) + " has " + str(self.usage.free) + " space for " + str(self.totalplots) + " total plots")
             
     def recalculateUsage(self):
         self.usage = psutil.disk_usage(memory.mountpoint)
@@ -162,13 +173,16 @@ class memoryClass(object):
         
         
 class winWorkloadThread(threading.Thread):
-    def __init__(self, string_command, workerID):
+    # TODO: add plot information to this class
+    def __init__(self, string_command, workerID, logger):
         threading.Thread.__init__(self)
         self.what_to_run_string = string_command
+        self.workerID = workerID
+        self.logger = logger
 
     def run(self):
         new_string = "start /wait C:\\Windows\\system32\\cmd.exe /c " + self.what_to_run_string + ""
-        print new_string + "\n"
+        self.logger.info("'" + new_string + "'")
         os.system(new_string)
 
         
@@ -187,71 +201,95 @@ def SendEmail(receiver, subject, message, senderName="youremail"):
 # Main                                                    #
 ###########################################################
 def main(argv):
-    # TODO: Use logger to capture time
+    global DEBUG
 
     # Command line arguments
     parser = argparse.ArgumentParser(description='ERROR: Invalid inputs')
     parser.add_argument('--mNetworkEmail',default=False,type=bool,metavar="mNetworkEmail",help="Use this flag if you want email updates on plots")
     parser.add_argument('--staggerMin',default=10,type=int,metavar="staggerMin",help="Stagger time between plots in minutes")
+    parser.add_argument('--sleepMin',default=10,type=int,metavar="sleepMin",help="Sleep time when checking plots")
+    parser.add_argument('--parallelPlots',default=8,type=int,metavar="parallelPlots",help="How many total plots run in parallel")
     parser.add_argument('--email',default="youremail@gmail.com",type=str,metavar="email",help="Stagger time between plots in minutes")
+    parser.add_argument('--DEBUG',default=False,type=bool,metavar="DEBUG",help="Use to test in DEBUG mode. DOES NOT generate plots")
+    
+    # TODO: Pass in public farmer/pool keys for David
     args = parser.parse_args()
 
     # Get arguments
     emailUpdateFlag = args.mNetworkEmail
     staggerTimeMin = args.staggerMin
+    sleepTimeMin = args.sleepMin
+    parallelPlots = args.parallelPlots
     emailAddress = args.email
+    DEBUG = args.DEBUG
+    
+    # TODO: Use logger for timestamps
+    logger = logging.getLogger(__name__)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    isDst = time.daylight and time.localtime().tm_isdst > 0
+    tzOffset = '-%02d' % ((time.altzone if isDst else time.timezone) / 3600)
+    formatter = logging.Formatter('%(asctime)s' + tzOffset + ' %(levelname) -8s %(module)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    if DEBUG:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
 
     # Determine if Windows / Linux
     if sys.platform.startswith('win'):
-        thisPC = winPC()
+        thisPC = winPC(logger, parallelPlots)
     else:
         emailUpdateFlag = False
         raise Exception("Linux plotBoy not supported yet!")
     
+    # TEST
+    # return
+    
     workloads = []
     WorkerID = 1
     PlotCompleted = 1
-    for x in range(0, thisPC.total_processes):
-        workloads.append(winWorkloadThread(thisPC.stringCmds[WorkerID-1], "WorkerID " + str(WorkerID)))
+    for x in range(0, thisPC.total_processes + 1):
+        workloads.append(winWorkloadThread(thisPC.stringCmds[WorkerID-1], WorkerID, logger))
         WorkerID += 1
         
     # Kick off plotting processes
     plotID = 1
     for workload in workloads:
-        print "Plot " + str(plotID) + " started"
+        logger.info("Plot " + str(plotID) + " started")
         workload.start()
         plotID += 1
         # TODO: Stagger by plotting stage
         time.sleep(staggerTimeMin * 60)       # Stagger time in minutes
         
     # Monitor Workloads
-    while WorkerID < thisPC.total_plots:
-        # Check every 10 min
-        time.sleep(10*60)
-        # time.sleep(60)
+    while len(workloads) > 0:
+    # while WorkerID < thisPC.total_plots:
+        # Check sleepTimeMin amount of minutes
+        time.sleep(sleepTimeMin*60)
         for x in range(0, len(workloads)):
             if not workloads[x].is_alive():
                 # Email that plot is done if email flag is set
                 if emailUpdateFlag:
                     emailMsg = "Plot " + str(PlotCompleted) + " is done on " + str(socket.gethostname()) + "!"
                     SendEmail(receiver=emailAddress, subject=emailMsg, message=emailMsg, senderName=emailAddress)
-                # print "workloads[" + str(x) + "] has finished"
+                logger.info("Plot " + str(workloads[x].workerID) + " has finished")
                 workloads.remove(workloads[x])
-                thisWorkload = winWorkloadThread(thisPC.stringCmds[WorkerID-1], "WorkerID " + str(WorkerID))
-                print "Plot " + str(plotID) + " started"
-                thisWorkload.start()
-                workloads.append(thisWorkload)
-                WorkerID += 1
-                plotID += 1
+                if WorkerID < thisPC.total_plots:
+                    thisWorkload = winWorkloadThread(thisPC.stringCmds[WorkerID-1], WorkerID, logger)
+                    logger.info("Plot " + str(plotID) + " started")
+                    thisWorkload.start()
+                    workloads.append(thisWorkload)
+                    WorkerID += 1
+                    plotID += 1
                 PlotCompleted += 1
                 break
             # else:
-                # print "workloads[" + str(x) + "] still running"
-        
+                # logger.info("workloads[" + str(x) + "] still running")
+                
     
-    # # Don't Need
-    # for workload in workloads:
-        # workload.join()
+
     
         
 if __name__ == "__main__":
